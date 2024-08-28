27a2142d93d7942552d3f94ef783484631ae0e49
BarrozoL <barrozo.lucasp@gmail.com>
2024-08-28 18:07:28 +0100

    byebye

--- WildFind-server/server/config/index.js
+++ WildFind-server/server/config/index.js
@@ -24,7 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [FRONTEND_URL],
+      origin: [FRONTEND_URL, "http://localhost:5005", "http://localhost:5173"],
     })
   );
 
--- WildFind-server/server/models/Watch.model.js
+++ WildFind-server/server/models/Watch.model.js
@@ -11,6 +11,7 @@ const watchSchema = new Schema(
     name: { type: String, required: true },
     typeId: { type: Number, enum: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] },
     dangerLevel: String,
+    edible: String,
     image: String,
     description: { type: String, required: true },
     location: { type: String, required: true },
